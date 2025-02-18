fb298946dc7db33a59c65980b2fbaf2ef31b3f03
ruben asua <rubenasuasoto@gmail.com>
2025-02-18 14:03:39 +0100

    .

--- src/main/kotlin/com/es/Api_Rest_Segura2/ApiRestSegura2Application.kt
+++ src/main/kotlin/com/es/Api_Rest_Segura2/ApiRestSegura2Application.kt
@@ -1,9 +1,12 @@
 package com.es.Api_Rest_Segura2
 
+import com.es.Api_Rest_Segura2.security.RSAKeysProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(RSAKeysProperties::class)
 class ApiRestSegura2Application
 
 fun main(args: Array<String>) {
